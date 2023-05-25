c51a0218e43e633889a86be4f78b9f4b22b2b401
akildam <lu34u5tl>
2023-05-24 21:47:43 -0300

    Adicionada condição para iniciar novamente a busca de uma modal aberta, quando a modal é fechada.

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,6 @@
+// ******************************************************
+// Botão de "Baixar todos" da listagem de Tarefas
+// ******************************************************
 projeto = parseInt($('#AF8_PROJET').val().trim());
 $("tr:visible").each(function (index) {
     if (index == 0) return; // não coloca o botão na primeira linha
@@ -11,49 +14,6 @@ $("tr:visible").each(function (index) {
         + "</span>"
     );
 });
-
-// procura pela tabela #tableInteracoes a cada segundo
-var tableInteracoesInterval = setInterval(buscarModalAberta, 1000);
-function buscarModalAberta(){
-    // ao encontrar o #tableInteracoes
-    if($.trim($("#tableInteracoes").html())) {
-        console.log('#tableInteracoes carregado. Parando o "tableInteracoesInterval".');
-
-        clearInterval(tableInteracoesInterval);
-        // adiciona o botão para download
-        $('#tableInteracoes thead tr:nth-child(2) td:nth-child(2)').html(
-            'Documentos <button class="btnBaixarInteracoes btn btn-sm btn-success"> <i class="fa fa-download"></i> Baixar todos</button>'
-        );
-
-        // Obtêm todas as URL's para download
-        arquivosParaDownload = [];
-        protocolo = $('#PCP_PROTOCOLO').val();
-        contadorArquivos = 100; // Começa em 100 para arquivos do tipo Interação
-        $("#tableInteracoes tr").each(function(){
-            $(this).find('td:nth-child(2) a').each(function(index){
-                contadorArquivos++;
-                arquivo = $(this).attr('href');
-                if(arquivo != undefined){
-                    extensao = arquivo.substr(arquivo.lastIndexOf('.') + 1);
-                    novoNome = contadorArquivos + ' - ' + protocolo + ' - ' + projeto + '.' + extensao;
-                    arquivosParaDownload.push({
-                        projeto: projeto,
-                        protocolo: protocolo,
-                        url: arquivo,
-                        filename: novoNome
-                    });
-                }
-            });
-        });
-
-        // adiciona o evento de clique ao botão
-        $('.btnBaixarInteracoes').on('click', function(){
-            chrome.runtime.sendMessage({arquivos: arquivosParaDownload});
-        });
-    }
-}
-
-
 
 $('.btnArquivos').on('click', function () {
     id = $(this).attr('arquivos');
@@ -76,5 +36,58 @@ $('.btnArquivos').on('click', function () {
         }
     });
     chrome.runtime.sendMessage({arquivos: arquivosParaDownload});
-})
+});
 
+// ******************************************************
+// Botão de "Baixar todos" da tela "Detalhes da Tarefa"
+// ******************************************************
+var modalAbertaInterval = setInterval(verificaModalAberta, 1000);
+
+function verificaModalAberta() {
+    abriuModal = $('.modal-backdrop').length == 1;
+    if(abriuModal) {
+        clearInterval(modalAbertaInterval);
+        botaoBaixarTodosInteracoes();
+    }
+}
+
+$('.modal').click(function(e) {
+    // verifica se clicou fora da modal
+    if (!$(e.target).closest('.modal-content').length) {
+        // se clicou fora da modal, inicia novamente o teste "verificaModalAberta"
+        modalAbertaInterval = setInterval(verificaModalAberta, 1000);
+    }
+});
+
+function botaoBaixarTodosInteracoes() {
+    // adiciona o botão para download
+    $('#tableInteracoes thead tr:nth-child(2) td:nth-child(2)').html(
+        'Documentos <button class="btnBaixarInteracoes btn btn-sm btn-success"> <i class="fa fa-download"></i> Baixar todos</button>'
+    );
+
+    // Obtêm todas as URL's para download
+    arquivosParaDownload = [];
+    protocolo = $('#PCP_PROTOCOLO').val();
+    contadorArquivos = 100; // Começa em 100 para arquivos do tipo Interação
+    $("#tableInteracoes tr").each(function(){
+        $(this).find('td:nth-child(2) a').each(function(index){
+            contadorArquivos++;
+            arquivo = $(this).attr('href');
+            if(arquivo != undefined){
+                extensao = arquivo.substr(arquivo.lastIndexOf('.') + 1);
+                novoNome = contadorArquivos + ' - ' + protocolo + ' - ' + projeto + '.' + extensao;
+                arquivosParaDownload.push({
+                    projeto: projeto,
+                    protocolo: protocolo,
+                    url: arquivo,
+                    filename: novoNome
+                });
+            }
+        });
+    });
+
+    // adiciona o evento de clique ao botão
+    $('.btnBaixarInteracoes').on('click', function(){
+        chrome.runtime.sendMessage({arquivos: arquivosParaDownload});
+    });
+}
